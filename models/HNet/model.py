61ca4e4a6bd3214605707ee9abb25ade98c68abd
zjubreathe <3220103352@zju.edu.cn>
2025-08-24 15:50:26 +0800

    基于空间均匀模糊核的神经网络

--- models/HNet/model.py
+++ models/HNet/model.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from pyexpat import features
+
+from fontTools.t1Lib import writePFB
 
 # 反卷积核
-from utils import Converse2D, LayerNorm
+from utils import Converse2D, LayerNorm, Converse2D_kernel, KernelNet
 
 class block(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, scale=1, padding=2, padding_mode='replicate',
@@ -24,6 +27,29 @@ class block(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x) + x
+        x = self.conv2(x) + x
+        return x
+
+class block_psf(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, scale=1, padding=2, padding_mode='replicate',
+                 eps=1e-5):
+        super(block_psf, self).__init__()
+        self.conv1_1 = nn.Sequential(LayerNorm(in_channels, eps=1e-5, data_format="channels_first"),
+                                   nn.Conv2d(in_channels, 2 * out_channels, 1, 1, 0),
+                                   nn.GELU())
+        self.conv1_2 = Converse2D_kernel(2 * out_channels)
+        self.conv1_3 =  nn.Sequential(nn.GELU(),
+                                      nn.Conv2d(2 * out_channels, out_channels, 1, 1, 0))
+        self.conv2 = nn.Sequential(LayerNorm(in_channels, eps=1e-5, data_format="channels_first"),
+                                   nn.Conv2d(out_channels, 2 * out_channels, 1, 1, 0),
+                                   nn.GELU(),
+                                   nn.Conv2d(2 * out_channels, out_channels, 1, 1, 0))
+
+    def forward(self, x, p):
+        x_ = x
+        x = self.conv1_1(x)
+        x = self.conv1_2(x,p,sf=1)
+        x = self.conv1_3(x) + x_
         x = self.conv2(x) + x
         return x
 
@@ -91,16 +117,97 @@ class HNet(nn.Module):
 
         return x[:, :, :H, :W]
 
+class HNet_a(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, features: int = 32, num_levels: int = 4):
+        super(HNet_a, self).__init__()
+
+        self.intro  = nn.Conv2d(in_channels, features, kernel_size=3, padding=1, stride=1, groups=1, bias=True)
+        self.ending = nn.Conv2d(features, out_channels, kernel_size=3, padding=1, stride=1, groups=1, bias=True)
+
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.conv_encs = nn.ModuleList()
+        self.conv_decs = nn.ModuleList()
+
+        self.kernel = KernelNet(kernel_size=7)
+
+        # Encoder
+        for num in range(num_levels):
+            self.encoders.append(
+                block_psf(features,features)
+            )
+            self.downs.append(
+                nn.Conv2d(features, 2*features, 2, 2)
+            )
+            self.conv_encs.append(nn.Conv2d(16, features*2, 1, 1, 0))
+            features = features * 2
+
+        # Bottleneck
+        self.bottleneck = block_psf(features, features)
+        self.conv_bot = nn.Conv2d(16, features*2, 1, 1, 0)
+
+        # Decoder
+        for num in range(num_levels):
+            self.ups.append(
+                nn.Sequential(
+                    nn.Conv2d(features, 2*features, 1, bias=False),
+                    nn.PixelShuffle(2)
+                )
+            )
+            features = features // 2
+            self.decoders.append(
+                    block_psf(features, features)
+            )
+            self.conv_decs.append(nn.Conv2d(16, features*2, 1, 1, 0))
+
+    def forward(self, x: torch.Tensor, p:torch.Tensor) -> torch.Tensor:
+        B, C, H, W = x.shape
+        x = self.intro(x)
+        enc_skips = []
+
+        b, c, h, w = p.shape
+        p_ = self.kernel(p)
+
+        # Encoder path
+        for encoder, down in zip(self.encoders, self.downs):
+            p = self.conv_encs[len(enc_skips)](p_)
+            x = encoder(x,p)
+            enc_skips.append(x)
+            x = down(x)
+
+        # Bottleneck
+        p = self.conv_bot(p_)
+        x = self.bottleneck(x,p)
+
+        levels = len(enc_skips)
+
+        # Decoder path
+        for decoder,up, enc_skip in zip(self.decoders, self.ups, enc_skips[::-1]):
+            p = self.conv_decs[levels-len(enc_skips)](p_)
+            enc_skips.pop()
+            x = up(x)
+            x = x + enc_skip
+            x = decoder(x,p)
+
+        x = self.ending(x)
+
+        return x[:, :, :H, :W]
+
+
+
 if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = HNet(in_channels=1, out_channels=1, features=32).to(device)
+    model = HNet_a(in_channels=1, out_channels=1, features=32).to(device)
 
     test_input = torch.randn(1, 1, 256, 256).to(device)
+    psf_input = torch.randn(1, 1, 7, 7).to(device)
 
     with torch.no_grad():
-        output = model(test_input)
+        output = model(test_input,psf_input)
 
     print(f"输入形状: {test_input.shape}")
     print(f"输出形状: {output.shape}")
--- utils.py
+++ utils.py
@@ -267,9 +267,6 @@ class ConverseBlockAlphaVariant(nn.Module):
         x = self.alpha2 * self.conv2(x) + x
         return x
 
-
-
-
 class ResidualBlock(nn.Module):
     """Residual block
     ---Conv-ReLU-Conv-+-
@@ -285,3 +282,115 @@ class ResidualBlock(nn.Module):
         out = F.relu(self.conv1(x), inplace=True)
         out = self.conv2(out)
         return x + out
+
+class KernelNet(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(KernelNet, self).__init__()
+        self.kernel_size = kernel_size
+        self.fc1 = nn.Linear(kernel_size ** 2, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 16 * (kernel_size ** 2))
+        self.gelu = nn.GELU()
+
+    def forward(self, k):
+        k = k.to(torch.float)
+        b, _, _, _ = k.shape
+        k = k.view(b, -1)  # flatten
+        # fully connected
+        k = self.gelu(self.fc1(k))
+        k = self.gelu(self.fc2(k))
+        k = self.fc3(k)
+        k = k.view(b, 16, self.kernel_size, self.kernel_size)
+        return k
+
+class Converse2D_kernel(nn.Module):
+    def __init__(self, features, eps=1e-3):
+        super(Converse2D_kernel, self).__init__()
+        '''
+        Converse2d operator for condition the kernel
+        '''
+        self.alpha = nn.Parameter(torch.zeros(1, features, 1, 1))
+        self.eps = eps
+
+    def forward(self, x, k, sf, padding=0, padding_mode='circular'):
+
+        if padding > 0:
+            x = nn.functional.pad(x, pad=[padding, padding, padding, padding], mode=padding_mode, value=0)
+        self.biaseps = torch.sigmoid(self.alpha - 9.0) + self.eps
+        _, _, h, w = x.shape
+        STy = self.upsample(x, sf)
+        if sf != 1:
+            x = nn.functional.interpolate(x, scale_factor=sf, mode='nearest')
+
+        FB = self.p2o(k, (h * sf, w * sf))
+        FBC = torch.conj(FB)
+        F2B = torch.pow(torch.abs(FB), 2)
+
+        FBFy = FBC * torch.fft.fftn(STy, dim=(-2, -1))
+        FR = FBFy + torch.fft.fftn(self.biaseps * x, dim=(-2, -1))
+        x1 = FB.mul(FR)
+        FBR = torch.mean(self.splits(x1, sf), dim=-1, keepdim=False)
+        invW = torch.mean(self.splits(F2B, sf), dim=-1, keepdim=False)
+        invWBR = FBR.div(invW + self.biaseps)
+        FCBinvWBR = FBC * invWBR.repeat(1, 1, sf, sf)
+        FX = (FR - FCBinvWBR) / self.biaseps
+        out = torch.real(torch.fft.ifftn(FX, dim=(-2, -1)))
+
+        if padding > 0:
+            out = out[..., padding * sf:-padding * sf, padding * sf:-padding * sf]
+
+        return out
+
+    def splits(self, a, scale):
+        '''
+        Split tensor `a` into `scale x scale` distinct blocks.
+        Args:
+            a: Tensor of shape (..., W, H)
+            scale: Split factor
+        Returns:
+            b: Tensor of shape (..., W/scale, H/scale, scale^2)
+        '''
+        *leading_dims, W, H = a.size()
+        W_s, H_s = W // scale, H // scale
+
+        # Reshape to separate the scale factors
+        b = a.view(*leading_dims, scale, W_s, scale, H_s)
+
+        # Generate the permutation order
+        permute_order = list(range(len(leading_dims))) + [len(leading_dims) + 1, len(leading_dims) + 3,
+                                                          len(leading_dims), len(leading_dims) + 2]
+        b = b.permute(*permute_order).contiguous()
+
+        # Combine the scale dimensions
+        b = b.view(*leading_dims, W_s, H_s, scale * scale)
+        return b
+
+    def p2o(self, psf, shape):
+        '''
+        Convert point-spread function to optical transfer function.
+        otf = p2o(psf) computes the Fast Fourier Transform (FFT) of the
+        point-spread function (PSF) array and creates the optical transfer
+        function (OTF) array that is not influenced by the PSF off-centering.
+        Args:
+            psf: NxCxhxw
+            shape: [H, W]
+        Returns:
+            otf: NxCxHxWx2
+        '''
+        otf = torch.zeros(psf.shape[:-2] + shape).type_as(psf)
+        otf[..., :psf.shape[-2], :psf.shape[-1]].copy_(psf)
+        otf = torch.roll(otf, (-int(psf.shape[-2] / 2), -int(psf.shape[-1] / 2)), dims=(-2, -1))
+        otf = torch.fft.fftn(otf, dim=(-2, -1))
+
+        return otf
+
+    def upsample(self, x, scale=3):
+        '''s-fold upsampler
+        Upsampling the spatial size by filling the new entries with zeros
+        x: tensor image, NxCxWxH
+        '''
+        st = 0
+        z = torch.zeros((x.shape[0], x.shape[1], x.shape[2] * scale, x.shape[3] * scale)).type_as(x)
+        z[..., st::scale, st::scale].copy_(x)
+        return z
+
